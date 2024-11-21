d02b3210460defca1a94d0b2ebf57370e7fa0eda
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2024-11-21 15:34:08 +0300

    add service section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'bgServices': "url('/src/img/bgServices.svg')",
+      }
+    },
   },
   plugins: [],
 }
